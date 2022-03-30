376cee5e46ced80f9d44f271293ce4e6cfd11c57
Sebastien Deriaz <sebastien.deriaz1@gmail.com>
2022-03-30 20:00:09 +0200

    test files

---
+++ SDPA_MR_OFDM/tests/test_general.py
@@ -0,0 +1,7 @@
+from SDPA_MR_OFDM.modulator import mr_ofdm_modulator
+
+def test_instanciation():
+    """
+    Tests if it is possible to instantiate the MR-OFDM modulator 
+    """
+    modulator = mr_ofdm_modulator()
